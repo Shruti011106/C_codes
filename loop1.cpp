eebaa36de5c8d817cb3cd260d0cfcce0a37246b3
Shruti011106 <shruti.cs241313@global.org.in>
2025-09-08 15:33:06 -0700

    loop1

--- loop1.cpp
+++ loop1.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    for (int i=0;i<2;i++){
-        for (int j=0;j<3;j++){
+    for (int i=0;i<3;i++){
+        for (int j=3;j>i;j--){
             printf("*");
         }
 printf("\n");
