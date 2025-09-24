bf76ebb74dc2698ffe4211eee85dd59e67e14fff
Shruti011106 <shruti.cs241313@global.org.in>
2025-09-24 13:23:56 -0700

    array2

---
+++ array2.cpp
@@ -0,0 +1,67 @@
+// #include<stdio.h>
+// int main(){
+//     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//     {
+//         printf("%d ", arr[i][j]);
+//         }
+//         printf("\n");
+        
+//     }
+    
+//     }
+// // #include<stdio.h>
+// // int main(){
+    
+// //     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+// //     int rev[3][3];
+// //     for (int i = 0; i < 3; i++)
+// //     {
+// //         for (int j = 2,k=0; j >=0 ,k<3; j--,k++)
+// //         {
+// //             rev[i][k]=arr[i][j];
+// //         }
+        
+// //     }
+// // for (int i = 0; i < 3; i++)
+// //     {
+// //         for (int k=0;  k<3; k++)
+// //         {
+// //             printf("%d ",rev[i][k]);
+// //         }
+// //         printf("\n");
+// //     }
+// //     return 0;
+
+// // }
+#include<stdio.h>
+int main(){
+    
+    int arr[3][4] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+        for (int i=0, j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+         printf("%d ",arr[1][2]);
+    for (int i=2, j = 2; j >=0; j--)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        for (int i=1, j = 0; j < 2; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+    return 0;
+
+}
