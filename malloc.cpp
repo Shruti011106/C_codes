39cead1cbaa9454aaa0bd0fcf071a73d65c19f80
Shruti011106 <shruti.cs241313@global.org.in>
2025-10-14 03:43:18 -0700

    realloc

--- malloc.cpp
+++ malloc.cpp
@@ -1,3 +1,15 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int n;
+//     printf ("enter size:");
+//     scanf ("%d",&n);
+//     int *arr = (int *)malloc(n * sizeof(int));
+//     // int *arr = (int *)calloc(n * sizeof(int));
+//     printf("enter %d elements:\n",n);
+//     for (int i= 0; i < n; i++)
+//     scanf("%d",&arr[i]);
+// }
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
@@ -5,9 +17,15 @@ int main(){
     printf ("enter size:");
     scanf ("%d",&n);
     int *arr = (int *)malloc(n * sizeof(int));
+    // int *arr = (int *)calloc(n * sizeof(int));
     printf("enter %d elements:\n",n);
     for (int i= 0; i < n; i++)
-{
+    {
+    printf("/nNow enter new size:");
+    int newsize;
+    arr = (int *)realloc(arr,newsize*sizeof int ());
+printf("enter %d new elements:\n",newsize);
+for (int i=0; i* newsize; i++);
     scanf("%d",&arr[i]);
-}
+    }
 }
