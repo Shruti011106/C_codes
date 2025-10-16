9494ed64da75cee7cedd17bded36cd6db9b5f73c
Shruti011106 <shruti.cs241313@global.org.in>
2025-10-16 03:41:43 -0700

    trap water

--- malloc.cpp
+++ malloc.cpp
@@ -1,3 +1,15 @@
+// // #include<stdio.h>
+// // #include<stdlib.h>
+// // int main(){
+// //     int n;
+// //     printf ("enter size:");
+// //     scanf ("%d",&n);
+// //     int *arr = (int *)malloc(n * sizeof(int));
+// //     // int *arr = (int *)calloc(n * sizeof(int));
+// //     printf("enter %d elements:\n",n);
+// //     for (int i= 0; i < n; i++)
+// //     scanf("%d",&arr[i]);
+// // }
 // #include<stdio.h>
 // #include<stdlib.h>
 // int main(){
@@ -8,24 +20,12 @@
 //     // int *arr = (int *)calloc(n * sizeof(int));
 //     printf("enter %d elements:\n",n);
 //     for (int i= 0; i < n; i++)
+//     {
+//     printf("/nNow enter new size:");
+//     int newsize;
+//     arr = (int *)realloc(arr,newsize*sizeof int ());
+// printf("enter %d new elements:\n",newsize);
+// for (int i=0; i* newsize; i++);
 //     scanf("%d",&arr[i]);
+//     }
 // }
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    int n;
-    printf ("enter size:");
-    scanf ("%d",&n);
-    int *arr = (int *)malloc(n * sizeof(int));
-    // int *arr = (int *)calloc(n * sizeof(int));
-    printf("enter %d elements:\n",n);
-    for (int i= 0; i < n; i++)
-    {
-    printf("/nNow enter new size:");
-    int newsize;
-    arr = (int *)realloc(arr,newsize*sizeof int ());
-printf("enter %d new elements:\n",newsize);
-for (int i=0; i* newsize; i++);
-    scanf("%d",&arr[i]);
-    }
-}
