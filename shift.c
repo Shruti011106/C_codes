af985f216539d8c986485cb676082c911c45c636
Shruti011106 <shruti.cs241313@global.org.in>
2025-10-30 03:54:48 -0700

    shift

---
+++ shift.c
@@ -0,0 +1,16 @@
+//right shift
+// #include<stdio.h>
+// int main()
+// {
+//   int a=10;
+//   int res=a>>1;
+//   printf("%d",res) ; 
+// }
+//left shift
+#include<stdio.h>
+int main()
+{
+  int a=5;
+  int res=a<<1;
+  printf("%d",res) ; 
+}
