94dea141d4794c8d7ef733ed9c319eb77f5b1427
Shruti011106 <shruti.cs241313@global.org.in>
2025-10-30 03:29:00 -0700

    xor

---
+++ afterswap.C
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5, b = 9;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout << "After swapping: a=" << a << ", b=" << b;
+    return 0;
+}
+// 5=101
+// 3=011
+// a=110
+//   011
+// b=101=5
+//   110
+// a=011=3
